0d9f64a773a933b2645d01387bb08b9f243faa1e
Kajal4414 <81718060+Kajal4414@users.noreply.github.com>
2024-07-09 15:52:40 +0000

    Add build.yml and update build.sh
    Change-Id: I5c0a99cd9d284838cb5e9165055835f1cd740821
    Signed-off-by: Kajal4414 <81718060+Kajal4414@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -1,77 +1,43 @@
 #!/bin/bash
-#
-# Compile script for uvite Kernel
-# Copyright (C) 2020-2021 Adithya R.
 
-SECONDS=0 # builtin bash timer
-ZIPNAME="uvite-$(date '+%Y%m%d-%H%M')-spes.zip"
-TC_DIR="$(pwd)/tc/clang-r450784e"
-AK3_DIR="$(pwd)/android/AnyKernel3"
-DEFCONFIG="vendor/spes-perf_defconfig"
+# Install Dependencies
+sudo apt install -y elfutils libarchive-tools flex bc cpio
 
-if test -z "$(git rev-parse --show-cdup 2>/dev/null)" &&
-   head=$(git rev-parse --verify HEAD 2>/dev/null); then
-	ZIPNAME="${ZIPNAME::-4}-$(echo $head | cut -c1-8).zip"
+# Clone Clang
+if [ ! -d "clang-r450784e" ]; then
+	git clone --depth 1 https://gitlab.com/ThankYouMario/android_prebuilts_clang-standalone.git clang-r450784e
 fi
 
-export PATH="$TC_DIR/bin:$PATH"
+# Set Environment Variables
 export KBUILD_BUILD_USER=nobody
 export KBUILD_BUILD_HOST=android-build
+export PATH="$(pwd)/clang-r450784e/bin:$PATH"
 
-if ! [ -d "$TC_DIR" ]; then
-	echo "AOSP clang not found! Cloning to $TC_DIR..."
-	if ! git clone --depth=1 -b 14 https://gitlab.com/ThankYouMario/android_prebuilts_clang-standalone "$TC_DIR"; then
-		echo "Cloning failed! Aborting..."
-		exit 1
-	fi
-fi
+# Cleanup
+rm -rf out build.log KernelSU AnyKernel3 *.zip
 
-if [[ $1 = "-r" || $1 = "--regen" ]]; then
-	make O=out ARCH=arm64 $DEFCONFIG savedefconfig
-	cp out/defconfig arch/arm64/configs/$DEFCONFIG
-	echo -e "\nSuccessfully regenerated defconfig at $DEFCONFIG"
-	exit
-fi
-
-if [[ $1 = "-rf" || $1 = "--regen-full" ]]; then
-	make O=out ARCH=arm64 $DEFCONFIG
-	cp out/.config arch/arm64/configs/$DEFCONFIG
-	echo -e "\nSuccessfully regenerated full defconfig at $DEFCONFIG"
-	exit
-fi
-
-if [[ $1 = "-c" || $1 = "--clean" ]]; then
-	rm -rf out
-fi
-
-mkdir -p out
-make O=out ARCH=arm64 $DEFCONFIG
-
-echo -e "\nStarting compilation...\n"
-make -j$(nproc --all) O=out ARCH=arm64 LLVM=1 LLVM_IAS=1 Image.gz dtbo.img 2> >(tee log.txt >&2) || exit $?
-
-kernel="out/arch/arm64/boot/Image.gz"
-dtbo="out/arch/arm64/boot/dtbo.img"
-
-if [ -f "$kernel" ]; then
-	echo -e "\nKernel compiled succesfully! Zipping up...\n"
-	if [ -d "$AK3_DIR" ]; then
-		cp -r $AK3_DIR AnyKernel3
-	elif ! git clone -q https://github.com/CHRISL7/AnyKernel3 -b master; then
-		echo -e "\nAnyKernel3 repo not found locally and couldn't clone from GitHub! Aborting..."
-		exit 1
-	fi
-	cp $kernel $dtbo AnyKernel3
-	rm -rf out/arch/arm64/boot
-	cd AnyKernel3
-	git checkout master &> /dev/null
-	zip -r9 "../$ZIPNAME" * -x .git README.md *placeholder
-	cd ..
-	rm -rf AnyKernel3
-	echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
-	echo "Zip: $ZIPNAME"
+# Integrate KernelSU
+echo -e -n "\e[33mDo you want to integrate KernelSU? (y/N):\e[0m " && read integrate_kernelsu
+if [ "$integrate_kernelsu" = "y" ]; then
+	git fetch https://github.com/Kajal4414/kernel_xiaomi_sm6225.git uvite-dev
+	git cherry-pick a172dcd
+	curl -LSs "https://raw.githubusercontent.com/Kajal4414/KernelSU/main/kernel/setup.sh" | bash -
+	ZIP_SUFFIX="SU"
 else
-	echo -e "\nCompilation failed!"
-	exit 1
+	ZIP_SUFFIX=""
 fi
 
+# Build Kernel
+make O=out ARCH=arm64 vendor/spes-perf_defconfig LLVM=1 LLVM_IAS=1 Image.gz dtbo.img -j$(nproc --all) 2> >(tee build.log >&2) || exit $?
+
+# Package Kernel
+if [ -f "out/arch/arm64/boot/Image.gz" ]; then
+	ZIPNAME="Uvite_Kernel${ZIP_SUFFIX}_$(date '+%d-%m-%Y')_$(git rev-parse --short=7 HEAD).zip"
+	git clone -q https://github.com/Kajal4414/AnyKernel3.git AnyKernel3
+	cp "out/arch/arm64/boot/Image.gz" "out/arch/arm64/boot/dtbo.img" AnyKernel3
+	(cd AnyKernel3 && zip -r9 "../$ZIPNAME" * -x .git README.md *placeholder)
+	if [ -f "$ZIPNAME" ]; then
+		echo -e "\e[32m\nCompleted in $((SECONDS / 60)) minutes and $((SECONDS % 60)) seconds!\e[0m"
+		echo -e "\e[32mZIP: $ZIPNAME\e[0m"
+	fi
+fi
